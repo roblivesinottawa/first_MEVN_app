9a28c3ddeec7b61ae6f4a6060faf9d89d039f040
Robson Trajano <59901422+roblivesinottawa@users.noreply.github.com>
2021-05-15 16:02:41 -0400

    moved and deleted files

--- backend/app/models/index.js
+++
@@ -1,9 +0,0 @@
-const dbConfig = require("../config/db.config")
-const mongoose = require("mongoose")
-
-const db = {}
-db.mongoose = mongoose
-db.url = dbConfig.url
-db.blog = require("./blog.model")(mongoose)
-
-module.exports = db
